a4c072f2a69ac73e4956035c1994e23ee0fb449c
Vishnu kumar <vk8273577360@gmail.com>
2023-08-27 18:21:37 +0530

    custom cursor added

--- js/common.js
+++ js/common.js
@@ -1,6 +1,6 @@
 const observer = new IntersectionObserver((entries) => {
   entries.forEach((entry) => {
-    console.log(entry);
+    // console.log(entry);
     if (entry.isIntersecting) {
       entry.target.classList.add("show");
     }
@@ -12,3 +12,23 @@ const observer = new IntersectionObserver((entries) => {
 
 const hiddenElements = document.querySelectorAll(".hidden");
 hiddenElements.forEach((element) => observer.observe(element));
+
+// custom cursor
+const container = document.getElementsByClassName("container")[0];
+const customCursor = document.getElementById("customCursor");
+const enlargeCursor = document.querySelectorAll(".enlargeCursor");
+
+container.addEventListener("mousemove", (dets) => {
+  // console.log(dets);
+  customCursor.style.left = dets.x - 5 + "px";
+  customCursor.style.top = dets.y - 5 + "px";
+});
+
+enlargeCursor.forEach((el) => {
+  el.addEventListener("mouseenter", () => {
+    customCursor.style.transform = "scale(20)";
+  });
+  el.addEventListener("mouseleave", () => {
+    customCursor.style.transform = "scale(1)";
+  });
+});
